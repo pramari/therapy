9ff3a0ac6c52ffcc85f898e8f57bb41e702ca955
Andreas <andreas@neumeier.org>
2026-07-24 18:15:59 +0000

    Initial commit.

---
+++ src/app.d.ts
@@ -0,0 +1,21 @@
+import type { User, Session } from 'better-auth';
+import { createAuth } from '$lib/server/auth';
+
+// See https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
+declare global {
+	namespace App {
+		interface Locals {
+			user?: User;
+			session?: Session;
+			auth: ReturnType<typeof createAuth>;
+		}
+
+		// interface Error {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
+
+export {};
